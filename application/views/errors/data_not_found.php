6089ddc5e1f90397038b190aab2491fa38dab678
havizIM <viz.ndinq@gmail.com>
2020-04-29 04:25:59 +0700

    Update Haviz

--- application/views/errors/data_not_found.php
+++ application/views/errors/data_not_found.php
@@ -6,7 +6,7 @@
             <h1 class="error-title">404</h1>
             <h3 class="text-uppercase error-subtitle">DATA NOT FOUND !</h3>
             <p class="text-muted m-t-30 m-b-30">THE DATA WITH THIS ID NOT FOUND IN OUR API </p>
-            <a href="javascript:void(0);" class="btn btn-primary btn-rounded waves-effect waves-light m-b-40">Back</a>
+            <a href="javascript:void(0);" class="btn btn-primary btn-rounded waves-effect waves-light m-b-40 btn-back">Back</a>
         </div>
     </div>
 
@@ -16,3 +16,11 @@
     All Rights Reserved by UD. Nusa Spring. Designed and Developed by
     <a href="https://wrappixel.com">CodeManiac</a>.
 </footer>
+
+<script>
+    $(document).ready(function(){
+        $('.btn-back').on('click', function(){
+            window.history.back();
+        })
+    })
+</script>
