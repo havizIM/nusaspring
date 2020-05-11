d01ecf3fafd45295fea5c05326a6a54f7e129284
havizIM <viz.ndinq@gmail.com>
2020-05-11 13:05:49 +0700

    Update haviz

--- src/modules/purchase.js
+++ src/modules/purchase.js
@@ -296,8 +296,7 @@ const purchaseUI = ((SET) => {
                                                         <img src="${SET.baseURL()}assets/images/logo-full-black.png" style="width: 50%" class="mb-3" />
                                                         <p class="text-muted m-l-5">Jl. Radar Auri No.2,
                                                             <br/> Cisalak Ps, Cimanggis, Depok, Jawa Barat 16452, Indonesia,
-                                                            <br/> Hp. 087880729929 / 081280999733,
-                                                            <br/> Telp/Fax. 021-29616935</p>
+                                                            <br/> Hp. 087880729929 / 081280999733</p>
                                                     </address>
                                                 </td>
                                                 <td class="w-50 text-right">
